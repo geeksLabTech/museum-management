4101b7cbfeb033ecc6d7b75c51ec6cf7bc03e2f7
Daniel <danielcardenascabrera2016@gmail.com>
2022-05-16 01:22:27 -0500

    Create_project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace museum_management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
