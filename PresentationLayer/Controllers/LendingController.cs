cbeb7b04dc7c34b8b53ff7e682a0cbbfc9af98a0
Daniel <danielcardenascabrera2016@gmail.com>
2022-06-18 22:57:46 -0400

    Fix_lending_view

--- PresentationLayer/Controllers/LendingController.cs
+++ PresentationLayer/Controllers/LendingController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DataLayer.Data;
+using PresentationLayer.ViewModels;
 //using System.Text;
 
 namespace museum_management.Controllers{
@@ -14,10 +15,24 @@ namespace museum_management.Controllers{
             }
     
             public async Task<IActionResult> Index() {
+                
                 var lendingToMuseums = await _context.LendingToMuseums.ToListAsync();
+                List<LendingViewModel> lendingViewModels = new List<LendingViewModel>();
+
+                foreach (var lending in lendingToMuseums) {
+                    var artwork = await _context.Artworks.FindAsync(lending.ArtworkId);
+                    var museum = await _context.Museums.FindAsync(lending.MuseumId);
+                    lendingViewModels.Add(new LendingViewModel {
+                        LendingToMuseum = lending,
+                        ArtworkTitle = artwork.Title,
+                        MuseumName = museum.Name,
+                        
+                    });
+                
+                }
                 System.Console.WriteLine(  "mmmm");
-                System.Console.WriteLine("LendingToMuseums: " + lendingToMuseums[0].Artwork.Title);
-                return View(lendingToMuseums);
+                System.Console.WriteLine("LendingToMuseums: " + lendingToMuseums[0].ArtworkId);
+                return View(lendingViewModels);
             }
     
     }
