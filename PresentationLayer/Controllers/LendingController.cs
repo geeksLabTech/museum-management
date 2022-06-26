c6161d250f22ea4f6632ef493b04062c15bc3477
Lia <liazerqueraf@gmail.com>
2022-06-26 12:08:13 -0400

    button rejec in lending done

--- PresentationLayer/Controllers/LendingController.cs
+++ PresentationLayer/Controllers/LendingController.cs
@@ -22,13 +22,8 @@ namespace museum_management.Controllers{
                 List<LendingViewModel> lendingViewModels = new List<LendingViewModel>();
 
                 foreach (var lending in lendingToMuseums) {
-                    System.Console.WriteLine("ID!!");
-                    System.Console.WriteLine(lending.ArtworkId);
-                    System.Console.WriteLine(lending.MuseumId);
                     var artwork = _unitOfWork.Artworks.GetById(lending.ArtworkId);
                     var museum = _unitOfWork.Museums.GetById(lending.MuseumId);
-                    System.Console.WriteLine(artwork == null);
-                    System.Console.WriteLine(museum == null);
                     lendingViewModels.Add(new LendingViewModel {
                         ArtworkId = lending.ArtworkId,
                         MuseumId = lending.MuseumId,
@@ -44,17 +39,9 @@ namespace museum_management.Controllers{
                 return View(lendingViewModels);
             }
             public IActionResult Edit(int artworkId, int museumId){
-                // var artwork = _unitOfWork.Artworks.GetById(Id);
-                System.Console.WriteLine("edITTT");
-                System.Console.WriteLine(artworkId);
-                System.Console.WriteLine(museumId);
                 var lending = _unitOfWork.Lendings.GetById(artworkId,museumId);
                 var artwork = _unitOfWork.Artworks.GetById(artworkId);
                 var museum = _unitOfWork.Museums.GetById(museumId);
-                System.Console.WriteLine(lending == null);
-                System.Console.WriteLine(artwork == null);
-                System.Console.WriteLine(museum == null);
-                System.Console.WriteLine();
                 var artworklendingViewModel = new ArtworklendingViewModel();
                 artworklendingViewModel.ArtworkId = lending.ArtworkId;
                 artworklendingViewModel.MuseumId = lending.MuseumId;
@@ -68,6 +55,13 @@ namespace museum_management.Controllers{
                    
                 // };
                 return View(artworklendingViewModel);
+            }
+            public IActionResult Reject(int artworkId, int museumId){
+                var lending = _unitOfWork.Lendings.GetById(artworkId,museumId);
+                lending.LendingState = LendingState.Denied;
+                _unitOfWork.Complete();
+                return RedirectToAction("Index");
+
             }
     
     }
