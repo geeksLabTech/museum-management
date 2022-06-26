5bce63337628d3d6b94c47b35322c7e5a286087d
Daniel <danielcardenascabrera2016@gmail.com>
2022-06-25 21:34:39 -0400

    Implement_new_views_for_delete_users_and_role_management

---
+++ PresentationLayer/ViewModels/ManageUserRolesViewModel.cs
@@ -0,0 +1,10 @@
+
+
+namespace PresentationLayer.ViewModels {
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
